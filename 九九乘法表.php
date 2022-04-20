ec5d1c2d49adbdc4fc0b8d12643709207d6a7f29
WuJiaoJue <103944313+WuJiaoJue@users.noreply.github.com>
2022-04-20 09:05:53 +0800

    Add files via upload

---
+++ 九九乘法表.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<table border="1" cellspacing="0">
+    <?php
+    for ($i = 1 ; $i <= 9 ; $i++ ){
+        echo "<tr>";
+        for ($j = 1 ; $j <= $i ; $j++ ){
+            echo "<td>".$i."*".$j."=".$i*$j."</td>";
+        }
+        echo "</tr>";
+    }
+    ?>
+
+</table>
+</body>
+</html>
